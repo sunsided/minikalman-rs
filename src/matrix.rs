588f4db1f524fd6c55187d55cc377fd4e3e5aa8b
Markus Mayer <widemeadows@gmail.com>
2022-12-30 20:58:09 +0100

    Add matrix-matrix multiplication

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,10 +1,20 @@
+use std::ptr::{null, null_mut};
+use stdint::{int_fast16_t, uint_fast16_t, uint_fast8_t};
+
 #[allow(non_camel_case_types)]
 type matrix_data_t = f32;
 
 pub struct Matrix<'a> {
-    pub rows: u8,
-    pub cols: u8,
+    pub rows: uint_fast8_t,
+    pub cols: uint_fast8_t,
     pub data: &'a mut [matrix_data_t],
+}
+
+/// Replaces `x` with `(x) as usize` to simplify index accesses.
+macro_rules! idx {
+    ( $x:expr ) => {
+        ($x) as usize
+    };
 }
 
 impl<'a> Matrix<'a> {
@@ -15,7 +25,7 @@ impl<'a> Matrix<'a> {
     /// * `rows` - The number of rows
     /// * `cols` - The number of columns
     /// * `buffer` - The data buffer (of size `rows` x `cols`).
-    pub fn new(rows: u8, cols: u8, buffer: &'a mut [matrix_data_t]) -> Self {
+    pub fn new(rows: uint_fast8_t, cols: uint_fast8_t, buffer: &'a mut [matrix_data_t]) -> Self {
         debug_assert_eq!(buffer.len(), (rows * cols) as _);
         Self {
             rows,
@@ -38,19 +48,62 @@ impl<'a> Matrix<'a> {
     pub fn invert_lower(&self, inverse: &mut Self) {
         debug_assert_eq!(self.rows, self.cols);
 
-        let n = self.rows as i16;
+        let n = self.rows;
         let t = self.data.as_ref();
         let a = inverse.data.as_mut();
 
         for i in 0..n {
-            let inv_el_ii = 1.0 / t[(i * n + i) as usize];
+            let inv_el_ii = 1.0 / t[idx!(i * n + i)];
             for j in 0..i {
-                let sum = (j..i).fold(0.0, |sum, k| {
-                    sum - t[(i * n + k) as usize] * a[(k * n + j) as usize]
-                });
-                a[(i * n + j) as usize] = sum * inv_el_ii;
+                let sum = (j..i).fold(0.0, |sum, k| sum - t[idx!(i * n + k)] * a[idx!(k * n + j)]);
+                a[idx!(i * n + j)] = sum * inv_el_ii;
             }
-            a[(i * n + i) as usize] = inv_el_ii;
+            a[idx!(i * n + i)] = inv_el_ii;
+        }
+    }
+
+    /**
+     * \brief Performs a matrix multiplication such that {\ref c} = {\ref a} * {\ref b}
+     * \param[in] a Matrix A
+     * \param[in] b Matrix B
+     * \param[in] c Resulting matrix C (will be overwritten)
+     * \param[in] aux Auxiliary vector that can hold a column of {\ref b}
+     *
+     * Kudos: https://code.google.com/p/efficient-java-matrix-library
+     */
+    pub fn mult(a: &Self, b: &Self, c: &mut Self, baux: &mut [matrix_data_t]) {
+        let bcols = b.cols;
+        let ccols = c.cols;
+        let brows = b.rows;
+        let arows = a.rows;
+
+        let adata = a.data.as_ref();
+        let cdata = c.data.as_mut();
+
+        // test dimensions of a and b
+        debug_assert_eq!(a.cols, b.rows);
+
+        // test dimension of c
+        debug_assert_eq!(a.rows, c.rows);
+        debug_assert_eq!(b.cols, c.cols);
+
+        // Test aux dimensions.
+        debug_assert_eq!(baux.len(), a.cols as _);
+        debug_assert_eq!(baux.len(), b.rows as _);
+
+        for j in (0..bcols).rev() {
+            // create a copy of the column in B to avoid cache issues
+            Self::get_column_copy(&b, j, baux);
+
+            let mut indexA: uint_fast16_t = 0;
+            for i in 0..arows {
+                let mut total = 0 as matrix_data_t;
+                for k in 0..brows {
+                    total += adata[idx!(indexA)] * baux[idx!(k)];
+                    indexA += 1;
+                }
+                cdata[idx!(i * ccols + j)] = total;
+            }
         }
     }
 
@@ -78,20 +131,6 @@ impl<'a> Matrix<'a> {
      * Kudos: https://code.google.com/p/efficient-java-matrix-library
      */
     pub fn multadd_rowvector(/* const matrix_t *RESTRICT const a, const matrix_t *RESTRICT const x, matrix_t *RESTRICT const c */
-    ) {
-        todo!()
-    }
-
-    /**
-     * \brief Performs a matrix multiplication such that {\ref c} = {\ref a} * {\ref b}
-     * \param[in] a Matrix A
-     * \param[in] b Matrix B
-     * \param[in] c Resulting matrix C (will be overwritten)
-     * \param[in] aux Auxiliary vector that can hold a column of {\ref b}
-     *
-     * Kudos: https://code.google.com/p/efficient-java-matrix-library
-     */
-    pub fn mult(/* const matrix_t *const a, const matrix_t *const b, const matrix_t *RESTRICT c, matrix_data_t *const baux */
     ) {
         todo!()
     }
@@ -204,27 +243,22 @@ impl<'a> Matrix<'a> {
      * \param[in] rows The column
      * \param[in] row_data Pointer to an array of the correct length to hold a column of matrix {\ref mat}.
      */
-    pub fn get_column_copy(/*const matrix_t *const mat, const register uint_fast8_t column, register matrix_data_t *const row_data*/
-    ) {
-        todo!()
-        /*
+    pub fn get_column_copy(mat: &Self, column: uint_fast8_t, row_data: &mut [matrix_data_t]) {
         // start from the back, so target index is equal to the index of the last row.
-        register uint_fast8_t target_index = mat->rows - 1;
+        let mut target_index: int_fast16_t = (mat.rows - 1) as _;
 
         // also, the source index is the column..th index
-        const register int_fast16_t stride = mat->cols;
-        register int_fast16_t source_index = target_index * stride + column;
+        let stride: int_fast16_t = mat.cols as _;
+        let mut source_index = (target_index as int_fast16_t) * stride + (column as int_fast16_t);
 
         // fetch data
-        row_data[target_index] = mat->data[source_index];
-        while (target_index != 0)
-        {
-        --target_index;
-        source_index -= stride;
+        row_data[idx!(target_index)] = mat.data[idx!(source_index)];
+        while target_index != 0 {
+            target_index -= 1;
+            source_index -= stride;
 
-        row_data[target_index] = mat->data[source_index];
+            row_data[idx!(target_index)] = mat.data[idx!(source_index)];
         }
-         */
     }
 
     /**
@@ -346,8 +380,12 @@ mod tests {
     use assert_float_eq::*;
 
     #[test]
+    #[rustfmt::skip]
     fn invert_lower() {
-        let mut a_buf = [1.0f32, 0.0, 0.0, -2.0, 1.0, 0.0, 3.5, -2.5, 1.0];
+        let mut a_buf = [
+             1.0,  0.0,  0.0,
+            -2.0,  1.0,  0.0,
+             3.5, -2.5,  1.0];
         let a = Matrix::new(3, 3, &mut a_buf);
 
         let mut inv_buf = [0f32; 3 * 3];
@@ -363,5 +401,29 @@ mod tests {
         assert_f32_near!(inv_buf[6], 1.5);
         assert_f32_near!(inv_buf[7], 2.5);
         assert_f32_near!(inv_buf[8], 1.0);
+    }
+
+    #[test]
+    #[rustfmt::skip]
+    fn mult_ident() {
+        let mut a_buf = [
+            1.0, 2.0, 3.0,
+            4.0, 5.0, 6.0];
+        let mut b_buf = [
+            10.0, 11.0,
+            20.0, 21.0,
+            30.0, 31.0];
+        let a = Matrix::new(2, 3, &mut a_buf);
+        let b = Matrix::new(3, 2, &mut b_buf);
+
+        let mut c_buf = [0f32; 2 * 2];
+        let mut c = Matrix::new(2, 2, &mut c_buf);
+
+        let mut aux = [0f32; 3 * 1];
+        Matrix::mult(&a, &b, &mut c, &mut aux);
+        assert_f32_near!(c_buf[0], 1. * 10. + 2. * 20. + 3. * 30.);
+        assert_f32_near!(c_buf[1], 1. * 11. + 2. * 21. + 3. * 31.);
+        assert_f32_near!(c_buf[2], 4. * 10. + 5. * 20. + 6. * 30.);
+        assert_f32_near!(c_buf[3], 4. * 11. + 5. * 21. + 6. * 31.);
     }
 }
