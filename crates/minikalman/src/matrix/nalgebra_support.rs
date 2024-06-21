c54c20b8b5679cbbe65bd60e1285417a232e9488
Markus Mayer <widemeadows@gmail.com>
2024-06-21 18:25:17 +0200

    Add RawStorage implementation
    Signed-off-by: Markus Mayer <widemeadows@gmail.com>

--- crates/minikalman/src/matrix/nalgebra_support.rs
+++ crates/minikalman/src/matrix/nalgebra_support.rs
@@ -1,4 +1,5 @@
-use nalgebra::IsContiguous;
+use nalgebra::allocator::Allocator;
+use nalgebra::{Const, DefaultAllocator, IsContiguous, Owned, RawStorage, Scalar, Storage};
 
 #[cfg_attr(docsrs, doc(cfg(all(feature = "nalgebra", feature = "unsafe"))))]
 #[cfg(all(feature = "nalgebra", feature = "unsafe"))]
@@ -30,6 +31,52 @@ unsafe impl<'a, const ROWS: usize, const COLS: usize, T> IsContiguous
     for crate::matrix::MatrixDataMut<'a, ROWS, COLS, T>
 {
 }
+
+// ---------
+
+#[cfg_attr(docsrs, doc(cfg(all(feature = "nalgebra", feature = "unsafe"))))]
+#[cfg(all(feature = "nalgebra", feature = "unsafe"))]
+unsafe impl<const ROWS: usize, const COLS: usize, const TOTAL: usize, T>
+    RawStorage<T, Const<ROWS>, Const<COLS>>
+    for crate::matrix::MatrixDataArray<ROWS, COLS, TOTAL, T>
+{
+    type RStride = Const<1>;
+    type CStride = Const<ROWS>;
+
+    #[inline]
+    fn ptr(&self) -> *const T {
+        self.as_ref().as_ptr()
+    }
+
+    #[inline]
+    fn shape(&self) -> (Const<ROWS>, Const<COLS>) {
+        (Const, Const)
+    }
+
+    #[inline]
+    fn strides(&self) -> (Self::RStride, Self::CStride) {
+        (Const, Const)
+    }
+
+    #[inline]
+    fn is_contiguous(&self) -> bool {
+        true
+    }
+
+    #[inline]
+    unsafe fn as_slice_unchecked(&self) -> &[T] {
+        std::slice::from_raw_parts(self.ptr(), ROWS * COLS)
+    }
+}
+
+/*
+#[cfg_attr(docsrs, doc(cfg(all(feature = "nalgebra", feature = "unsafe"))))]
+#[cfg(all(feature = "nalgebra", feature = "unsafe"))]
+unsafe impl<const ROWS: usize, const COLS: usize, const TOTAL: usize, T>
+    Storage<T, Const<ROWS>, Const<COLS>> for crate::matrix::MatrixDataArray<ROWS, COLS, TOTAL, T>
+{
+}
+*/
 
 /*
 impl<const ROWS: usize, const COLS: usize, T, S> crate::matrix::Matrix<ROWS, COLS, T> for S where
